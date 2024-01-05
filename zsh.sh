59ae86fd48a71bf94655bbf67bac59726cfd0ac6
Gnasoh <97218994+Gnasoh@users.noreply.github.com>
2024-01-05 18:24:49 +0900

    add zsh cofiguration sh

---
+++ zsh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get install zsh -y
+which zsh
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sudo apt-get install git
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlightinggit clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc
+echo -e "PROMPT='😒%F{blue}%n@%m  %F{yellow}%~ %f'" >> ~/.zshrc 
+
+source ~/.zshrc
