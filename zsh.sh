b90ed07a5aaa8ecf73c10172b41a8518aae7a1b4
Gnasoh <97218994+Gnasoh@users.noreply.github.com>
2024-01-08 11:07:25 +0900

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -2,12 +2,15 @@
 sudo apt-get install zsh -y
 which zsh
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-sudo apt-get install git
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" <<END
+yes
+END
+sudo apt-get install git -y
+
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlightinggit clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc
-echo -e "PROMPT='😒%F{blue}%n@%m  %F{yellow}%~ %f'" >> ~/.zshrc 
+echo -e "PROMPT='😒%F{blue}%n@%m  %F{yellow}%~ %f'" >> ~/.zshrc
 
 source ~/.zshrc
