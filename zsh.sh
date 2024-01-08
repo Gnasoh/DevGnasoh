5a03cc979547f2764783e86a1bd33e61df221820
Gnasoh <97218994+Gnasoh@users.noreply.github.com>
2024-01-08 11:21:17 +0900

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -3,7 +3,7 @@ sudo apt-get install zsh -y
 which zsh
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" <<END
-yes
+Y
 END
 sudo apt-get install git -y
 
